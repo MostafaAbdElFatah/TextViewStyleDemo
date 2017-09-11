960abb25c400629106145a5e6c7c2ef4c69e28e3
MostafaAbdElFatah <31865667+MostafaAbdElFatah@users.noreply.github.com>
2017-09-12 00:52:16 +0200

    Initial commit

---
+++ app/src/main/java/com/mostafaabdel_fatah/textviewstyledemo/MainActivity.java
@@ -0,0 +1,13 @@
+package com.mostafaabdel_fatah.textviewstyledemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
